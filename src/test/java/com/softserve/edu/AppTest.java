7446ce2d885266e5c3016d7d2f7edaec34818b9d
bohdana.yatsyshyn <bohdanayatsyshyn@eleks.com>
2024-02-04 20:00:54 +0200

    message updated

--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -7,7 +7,7 @@ public class AppTest {
 
     @Test
     public void testApp() {
-        System.out.println("first test. Updated12636");
+        System.out.println("first test. Enhanced");
         Assertions.assertTrue(true);
     }
 }
