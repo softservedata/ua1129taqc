1d0cb191aaa6a5e26ea90e88a89708af964aaab5
Oksana Kushnir <kushnir.oksanaa@gmail.com>
2024-02-08 17:01:14 +0200

    Created initial C# project

---
+++ BranchTask/BranchApp/BranchApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BranchApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
