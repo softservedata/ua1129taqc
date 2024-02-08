bfe63b92406c41a6a076627fa612ce365ac14b5a
Vira Zenkevych <zenk-vera@i.ua>
2024-02-08 16:36:23 +0200

    Message updated

--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -7,7 +7,7 @@ public class AppTest {
 
     @Test
     public void testApp() {
-        System.out.println("first test");
+        System.out.println("first test.Updated");
         Assertions.assertTrue(true);
     }
 }
