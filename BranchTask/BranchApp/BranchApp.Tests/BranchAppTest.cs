5d3d9425a7e25c0fd0f2260b5ed35d4874455153
Oksana Kushnir <kushnir.oksanaa@gmail.com>
2024-02-08 17:54:53 +0200

    Added Test project

---
+++ BranchTask/BranchApp/BranchApp.Tests/BranchAppTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace BranchApp.Tests
+{
+    [TestFixture]
+    public class BranchAppTest
+    {
+        [Test]
+        public void FirstAppTest()
+        {
+            Console.WriteLine("Oksana's first test!!!");
+            ClassicAssert.IsTrue(true);
+        }
+    }
+}
