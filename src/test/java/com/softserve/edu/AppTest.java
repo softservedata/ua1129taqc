88aed0cfe11d1f7b7a613fa68abd5476b29c6cf9
mismechanic <mis.mechanic@gmail.com>
2024-02-27 16:35:57 +0200

    Update

--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -8,7 +8,7 @@ public class AppTest {
     @Test
     public void testApp() {
 
-        System.out.println("first test.Update1111111111");
+        System.out.println("first test.Update11111111111");
         Assertions.assertTrue(true);
     }
 }
