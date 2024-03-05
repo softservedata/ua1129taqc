2ebda35b01344789f202b044eb3081af15c4de4a
Yuliya <valkovec_yulia@ukr.net>
2024-03-05 13:36:00 +0200

    Message Updated

--- src/main/java/homework07/javapart2/CollectionOfNumbers.java
+++ src/main/java/homework07/javapart2/CollectionOfNumbers.java
@@ -18,43 +18,74 @@ import java.util.Random;
 
 public class CollectionOfNumbers {
 
-    public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>();
+
+    public static ArrayList<Integer> allRandomElements() {
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
         Random rand = new Random();
+        for (int i = 0; i < 20; i++) {
+            int randomNumber = rand.nextInt(100) + 1;
+            randomNumbers.add(randomNumber);
+        } return randomNumbers;
+    }
 
-        for (int i=0; i<20; i++) {
-            int randomNumbers = rand.nextInt(100) + 1;
-            numbers.add(randomNumbers);
-            }
-        System.out.println("ArrayList: " + numbers);
-        System.out.println("Minimum number in the collection: " + Collections.min(numbers) +
-                "\nMaximum number in the collection: " + Collections.max(numbers));
-
+    public static double averageValueOfNumbers(ArrayList<Integer> numbers) {
         double sum = 0;
-        for(int j : numbers){
+        for (int j : numbers) {
             sum += sum + j;
         }
         double averageValue = sum / numbers.size();
-        System.out.println("Average value of the numbers in the collection: " + averageValue);
+        return averageValue;
+    }
 
+    public static ArrayList<Integer> removingEvenNumbers(ArrayList<Integer> numbers){
         Iterator<Integer> num = numbers.iterator();
         while (num.hasNext()) {
             Integer i = num.next();
             if(i % 2 == 0){
                 num.remove();
             }
-        } System.out.println("Collection without even numbers: " + numbers);
+        }
+        return numbers;
+    }
 
-            int givenNumber = 35;
-            boolean numberContains = numbers.contains(givenNumber);
-            if (numberContains){
-                System.out.println("The collection contains the given number " + givenNumber);
+    public static boolean checkGivenNumber(ArrayList<Integer> numbers){
+        int givenNumber = 35;
+        boolean numberContains = numbers.contains(givenNumber);
+        return numberContains;
+    }
+
+    public static ArrayList<Integer> sortCollection(ArrayList<Integer> numbers){
+        Collections.sort(numbers);
+        return numbers;
+    }
+
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numberCollection = allRandomElements();
+
+        // Print all elements of the collection to the console.
+        System.out.println("ArrayList: " + numberCollection);
+
+        // Find and print the minimum and maximum numbers in the collection.
+        System.out.println("\nMinimum number in the collection: " + Collections.min(numberCollection) +
+                "\nMaximum number in the collection: " + Collections.max(numberCollection));
+
+        // Calculate and print the average value of the numbers in the collection.
+        System.out.println("\nAverage value of the numbers in the collection: " + averageValueOfNumbers(numberCollection));
+
+        // Remove all even numbers from the collection, and then print the updated collection to the console.
+        System.out.println("\nCollection without even numbers: " + removingEvenNumbers(numberCollection));
+
+        // Check if the collection contains a given number and print the result to the console.
+        if (checkGivenNumber(numberCollection)){
+                System.out.println("\nThe collection contains the given number");
             } else {
-                System.out.println("The collection doesn't contain the given number: " + givenNumber);
+                System.out.println("\nThe collection doesn't contain the given number");
             }
 
-            Collections.sort(numbers);
-            System.out.println("Sorted collection: " + numbers);
+        // Sort the collection in ascending order and print the sorted collection to the console.
+            System.out.println("\nSorted collection: " + sortCollection(numberCollection));
         }
 
         }
