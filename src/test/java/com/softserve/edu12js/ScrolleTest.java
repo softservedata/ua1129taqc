baa3efb954027dbc06109ba3349623a8bfbaebe5
yharasym <garasym@gmail.com>
2024-03-07 19:59:04 +0200

    Implicit, Explicit and by JavaScript. Search WebElements

--- src/test/java/com/softserve/edu12js/ScrolleTest.java
+++ src/test/java/com/softserve/edu12js/ScrolleTest.java
@@ -15,12 +15,18 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class ScrolleTest {
     private static final String BASE_URL = "https://selenium.dev/downloads/";
     private static final Long IMPLICITLY_WAIT_SECONDS = 10L;
+    private static final Long SCRIPT_TIMEOUT_WAIT_SECONDS = 100L;
+    private static final Long PAGE_LOAD_TIMEOUT_WAIT_SECONDS = 180L;
     private static final long ONE_SECOND_DELAY = 1000;
     private static final String TIME_TEMPLATE = "yyyy-MM-dd_HH-mm-ss-S";
     private static WebDriver driver;
@@ -61,6 +67,17 @@ public class ScrolleTest {
         }
     }
 
+    private void scrollToWebElement(WebElement webElement) {
+        /*
+        // Scrolling by Action class
+        Actions action = new Actions(driver);
+        action.moveToElement(webElement).perform();
+        */
+        //
+        // Scrolling by JavaScript injection
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", webElement);
+    }
+
     @BeforeAll
     public static void setup() {
         WebDriverManager.chromedriver().setup();
@@ -75,6 +92,8 @@ public class ScrolleTest {
         //
         //driver.manage().timeouts().implicitlyWait(IMPLICITLY_WAIT_SECONDS, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICITLY_WAIT_SECONDS)); // 0 by default
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(SCRIPT_TIMEOUT_WAIT_SECONDS));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(PAGE_LOAD_TIMEOUT_WAIT_SECONDS));
         driver.manage().window().maximize();
         //driver.manage().window().setSize(new Dimension(1295, 687));
         System.out.println("@BeforeAll executed");
@@ -108,7 +127,7 @@ public class ScrolleTest {
         // Close Session
     }
 
-    @Test
+    //@Test
     public void checkScroll() throws Exception {
         //
         WebElement linkProject = driver.findElement(By.cssSelector("a[href='https://www.perfecto.io/']"));
@@ -123,9 +142,62 @@ public class ScrolleTest {
         //
         // Scrolling by JavaScript injection
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", linkProject);
+        presentationSleep(4); // For Presentation ONLY
         //
-        Assertions.assertTrue(false);
+        ((JavascriptExecutor)driver).executeScript("alert('Hello, click OK')");
+        presentationSleep(4); // For Presentation ONLY
+        //
+        driver.switchTo().alert().accept();
+        presentationSleep(); // For Presentation ONLY
+        //
+        Assertions.assertTrue(true);
         //
         isTestSuccessful = true;
+    }
+
+    @Test
+    public void checkFindElements() throws Exception {
+        //
+        WebElement linkProject = driver.findElement(By.cssSelector("a[href='https://www.perfecto.io/']"));
+        presentationSleep(4); // For Presentation Only
+        //
+        scrollToWebElement(linkProject);
+        presentationSleep(4); // For Presentation Only
+        //
+        //WebElement browserstack = driver.findElement(By.cssSelector("a[href*='www.browserstack.com']"));
+        //
+        // For Explicit Wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+        //
+        //WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(IMPLICITLY_WAIT_SECONDS));
+        //
+//        Wait<WebDriver> webDriverWait = new FluentWait<>(driver)
+//                .withTimeout(Duration.ofSeconds(10))
+//                .pollingEvery(Duration.ofMillis(50))
+//                .ignoring(NullPointerException.class)
+//                .ignoring(TimeoutException.class);
+        //
+        //WebElement browserstack = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href*='www.browserstack.com']")));
+        //
+        // Find by JavaScript
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement browserstack = (WebElement) (js.executeScript("return document.querySelector(\"a[href*='http://www.browserstack.com']\");"));
+        //
+        // For Implicit Wait
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICITLY_WAIT_SECONDS));
+        //
+        //
+        System.out.println("browserstack.isDisplayed() = " + browserstack.isDisplayed());
+        System.out.println("browserstack.isEnabled() = " + browserstack.isEnabled());
+        System.out.println("browserstack.isSelected() = " + browserstack.isSelected());
+        presentationSleep(4); // For Presentation Only
+        //
+        // Click by Selenium
+        //browserstack.click();
+        //
+        // Click by JavaSript
+        js.executeScript("arguments[0].click();", browserstack);
+        //
+        //Assertions.assertTrue(false);
     }
 }
