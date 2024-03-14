185aceb5cc13fd6f3de66a16bf0df3af034c4d00
Veronika <gav51@ukr.net>
2024-03-14 11:48:23 +0100

    Veronika updated

--- src/test/java/com/softserve/edu/JavaPart2HW/Tests/RectangleTest.java
+++ src/test/java/com/softserve/edu/JavaPart2HW/Tests/RectangleTest.java
@@ -4,15 +4,19 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.softserve.edu.JavaPart2HW.Rectangle;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RectangleTest {
 
-    @Test
-    public void rectangleTest() {
-        // Arrange
-        var width = 5;
-        var height = 5;
-
+    @ParameterizedTest
+    @CsvSource({
+            "5, 5",
+            "7, 7",
+            "9, 9"
+    })
+    public void rectangleTest(double height, double width) {
         // Act
         var rectangle = new Rectangle(width, height);
 
@@ -23,48 +27,55 @@ public class RectangleTest {
         Assertions.assertEquals(expectedStandardAngle, rectangle.getAngle());
     }
 
-    @Test
-    public void rectangleAreaTest() {
+    @ParameterizedTest
+    @CsvSource({
+            "5, 5, 25",
+            "7, 7, 49",
+            "9, 9, 81"
+    })
+    public void rectangleAreaTest(double width, double height, double expectedArea) {
         // Arrange
-        var width = 5;
-        var height = 5;
         var rectangle = new Rectangle(width, height);
 
         // Act
         var calculatedArea = rectangle.calculateArea();
 
         // Assert
-        var expectedArea = 25;
         Assertions.assertEquals(expectedArea, calculatedArea);
     }
 
-    @Test
-    public void rectanglePerimeterTest(){
+    @ParameterizedTest
+    @CsvSource({
+            "5, 3, 16",
+            "7, 4, 22",
+            "9, 7, 32"
+    })
+    public void rectanglePerimeterTest(double width, double height, double expectedPerimeter){
         // Arrange
-        var width = 5;
-        var height = 5;
         var rectangle = new Rectangle(width, height);
 
         //Act
         var calculatedPerimeter = rectangle.calculatePerimeter();
 
         //Assert
-        var expectedPerimeter = 20;
         Assertions.assertEquals(expectedPerimeter, calculatedPerimeter);
     }
 
-    @Test
-    public void rectangleDiagonalTest(){
+    @ParameterizedTest
+    @CsvSource({
+            "5, 3,5.830951894845301",
+            "7, 4, 8.06225774829855",
+            "9, 7, 11.40175425099138"
+    })
+    public void rectangleDiagonalTest(double width, double height, double expectedDiagonal){
         // Arrange
-        var width = 5;
-        var height = 5;
+
         var rectangle = new Rectangle(width, height);
 
         //Act
         var calculatedDiagonal = rectangle.getDiagonal();
 
         //Assert
-        var expectedDiagonal = 50;
         Assertions.assertEquals(expectedDiagonal, calculatedDiagonal);
     }
 }
